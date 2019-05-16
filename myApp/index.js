94097e7f5fe52f0335ab06087b72a35192bad198
dharanitharan <dharanitharan.it@gmail.com>
2019-05-16 11:06:11 +0530

    Attachement

--- myApp/index.js
+++ myApp/index.js
@@ -1,4 +1,15 @@
+var http = require('http');
 var nodemailer = require('nodemailer');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<form method="post" enctype="multipart/form-data">');
+  res.write('<input type="text" id="to"><br>');
+  res.write('<input type="button">');
+  res.write('</form>');
+  return res.end();
+}).listen(8080);
+
 
 var transporter = nodemailer.createTransport({
     pool: true,
@@ -6,18 +17,25 @@ var transporter = nodemailer.createTransport({
     port: 465,
     secure: true, // use SSL
     auth: {
-        user: 'dharanitharan.it@gmail.com',
-        pass: 'urxlrsojjdwcoayy'
+        user: 'dharanitharan.se@gmail.com',
+        pass: 'tfcweabxmfvjadrw'
     }
 });
 
 var mailOptions = {
-  from: 'dharanitharan.it@gmail.com',
-  to: 'venkatreddy3093712@gmail.com, dharani.infonet@gmail.com',
-  cc: 'ureshsrna4@gmail.com',
-  subject:'Dharanitharan',
+  from: '"Dharanitharan" <dharanitharan.se@gmail.com>',
+  to:  'dharanitharan.m@enterprisetouch.net',
+  //cc: 'ureshsrna4@gmail.com',
+  //bcc: 'ureshsrna4@gmail.com',
+  subject:'Node.Js Test mail - Dharanitharan',
   text: 'Hi Guys....! This is Dharanitharan',
-  html: '<h1 style="color:red">Welcome</h1><p>That was easy!</p>'
+  html: '<h1 id="uresh" style="color:red">Welcome</h1><p>That was easy!</p>',
+  attachments:
+   [  
+    {
+        path: './img/apple.png'
+    }
+  ]  
 };
 
 transporter.sendMail(mailOptions, function(error, info){
